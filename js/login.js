3d9859e689a14c48386fa311679902ea8d05072c
nembot <nguyen.am.stephane@gmail.com>
2014-12-29 16:19:40 +0100

    affichage des parties
    affichage des parties dans la page de login + lorsque qu’on clique sur
    la partie, le champ input de nouvelle partie se désactive pour mettre
    le nom de la partie cliquée.

--- js/login.js
+++ js/login.js
@@ -2,16 +2,28 @@ function init() {
 	// Connect to the SocketIO server to retrieve ongoing games.
 	socket = io.connect();
 	socket.on('gamesList', function(data) {
-								 var ul = document.getElementById('lesParties');
-								 ul.innerHTML='';
-								 for(p in data.gamesList) {
-									 var li = document.createElement('li'); 
-									 ul.appendChild( li );
-									 li.appendChild( document.createTextNode( data.gamesList[p] ) );
-									}
-								}
-			 );
-	socket.emit('loginPage');
+		var rejoindreUnePartie = document.getElementById('rejoindrepartie');
+		var ul = document.getElementById('lesParties');
+		ul.innerHTML='';
+		if (data.gamesList.length > 0) {
+			rejoindreUnePartie.innerHTML='Rejoindre une partie';
+		}
+		for(p in data.gamesList) {
+			var li = document.createElement('li');
+			var a = document.createElement('a');
+			ul.appendChild( li );
+			li.appendChild( a );
+			a.appendChild( document.createTextNode( data.gamesList[p] ) );
+			a.setAttribute('href', '#');
+			a.setAttribute('id', data.gamesList[p]);
+			a.setAttribute('onclick', 'putName("'+data.gamesList[p]+'")');
+		}
+	}
+);
+socket.emit('loginPage');
 }
 
-
+function putName(id) {
+	document.getElementById("idGame").value = id;
+	document.getElementById("idGame").setAttribute('readonly', '');
+}
