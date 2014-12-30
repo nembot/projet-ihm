69eb50ee4f0549424f89978df3111928cef4ce8d
massi <massi@success2i.com>
2014-12-30 12:59:28 +0100

    ajout du fichier deplacements.js

---
+++ js/deplacements.js
@@ -0,0 +1,351 @@
+
+var currentRobotColor;
+var currentRobotCellId;
+var currentRobotLine;
+var currentRobotColumn;
+
+
+ function addCellsListener() {
+ 	//console.log('adding cells listeners');
+ 	$("#tableJeu").click(function(e) {
+ 		var id = $(e.target).closest('td').attr('id');
+ 		
+ 		var robotCoordinates = getLineAndColumnFromId(id);
+ 		var line = robotCoordinates.line;
+ 		var column = robotCoordinates.column;
+
+ 		//resetCells();
+ 		if(getRobotColor(line, column) != "") {
+ 			demarquerRobot(currentRobotCellId);
+ 			currentRobotColor = getRobotColor(line, column);
+ 			currentRobotCellId = id;
+ 			currentRobotLine = line;
+ 			currentRobotColumn = column;
+ 			marquerRobot(id, currentRobotColor);
+
+ 		} 
+ 	});
+ }
+  
+ function resetCells() {
+ 	$("#"+currentRobotCellId).css('background-color', 'white');
+ 	$("#"+currentRobotCellId).empty();	
+ }
+ 
+ function initKeyboard() {
+ 	$('html').bind('keydown', function(e){
+ 		if(e.keyCode == 37 || e.keyCode == 38 ||e.keyCode == 39 ||e.keyCode == 40)
+ 			e.preventDefault();
+	    if(currentRobotColor != "") {
+	    	var nextCell;
+	    	if(e.keyCode == 38) {//up
+	    		nextCell = getNextPosition("up");
+	    	} else if(e.keyCode == 40) {//down
+	    		nextCell = getNextPosition("down");
+	    	} else if(e.keyCode == 39) {//right
+				nextCell = getNextPosition("right");
+	    	} else if(e.keyCode == 37) {// left
+	    		nextCell = getNextPosition("left");
+	    	}
+	    	if(nextCell) { // proposée
+	    		var id = nextCell.attr('id');
+	    		var l = getLineAndColumnFromId(id).line;
+	    		var c = getLineAndColumnFromId(id).column;
+	    		sendProposition(currentRobotColor, l, c);
+	    	}
+	    }
+	});
+ }
+
+
+ function updateRobotsPositions() {
+ 	for (var i = 0; i < robots.length; i++) {
+ 	 	if(robots[i].color == currentRobotColor) {
+ 	 		robots[i].line = currentRobotLine;
+ 	 		robots[i].column = currentRobotColumn;
+ 	 	}
+ 	 }
+ }
+
+ function getLineAndColumnFromId(id) {
+ 	var coordinates = {};
+
+ 	var lineStr = id.split('_')[0];
+ 	var colStr = id.split('_')[1];
+ 	var line = parseInt(lineStr.replace("i",""));
+ 	var column = parseInt(colStr.replace("j",""));
+ 	coordinates.line = line;
+ 	coordinates.column = column;
+ 	return coordinates;	
+ }
+
+  function moveRobot(cell) {
+  	//resetCells();
+  	var img = getRobotImg(currentRobotColor);
+  		cell.append(img);
+  }
+ /*
+	Dont touch : 
+
+	Calcule quelles sont les cases interdites lors du déplacement
+ */
+
+ function hasRobot(cell) {
+ 	var coordinates = getLineAndColumnFromId(cell.attr('id'));
+ 	 for (var i = 0; i < robots.length; i++) {
+ 	 	if(coordinates.line == robots[i].line && coordinates.column == robots[i].column) {
+ 	 		return true;
+ 	 	}
+ 	 }
+ 	 return false;
+ }
+
+ function getObstacles() {
+ 	var obstacles = {
+ 		h:-1, 
+ 		b:16,
+ 		g:-1,
+ 		d:16
+ 	};
+
+ 	for(var i = 0; i < board.length; i++) {//verical
+ 		var cell = $("#i"+i+"_j"+currentRobotColumn);
+ 			if(i < currentRobotLine && obstacles.h < i) { // obstacle haut
+ 				if(cell.hasClass("hautGras")) {
+ 					obstacles.h = i-1;
+ 				}  
+ 				if(cell.hasClass("basGras")) {
+ 					obstacles.h = i;
+ 				}
+ 				if(hasRobot(cell)) {
+ 					obstacles.h = i;	
+ 				}
+ 			} 
+
+ 			if(i > currentRobotLine && obstacles.b > i ) { // obstacle bas
+ 				if(cell.hasClass("hautGras")) {
+ 					obstacles.b = i;
+ 				} 
+ 				if(cell.hasClass("basGras")) {
+ 					obstacles.b = i+1;
+ 				}
+ 				if(hasRobot(cell)) {
+ 					obstacles.b = i;
+ 				}
+ 			}
+
+ 			if(i==currentRobotLine) {
+ 				if(cell.hasClass("hautGras")) {
+ 					obstacles.h = i-1;
+ 				} 
+ 				if(cell.hasClass("basGras")) {
+ 					obstacles.b = i+1;
+ 				}
+ 			}
+
+ 		}
+ 	
+
+ 	for(var j=0; j<board.length; j++) { // horizontal
+ 		var cell = $("#i"+currentRobotLine+"_j"+j);
+ 			if(j < currentRobotColumn && obstacles.g < j) { // obstacle gauche
+ 				if(cell.hasClass("gaucheGras")) {
+ 					obstacles.g = j-1;
+ 				} 
+ 				if(cell.hasClass("droiteGras")) {
+ 					obstacles.g = j;
+ 				}
+
+ 				if(hasRobot(cell)) {
+ 					obstacles.g = j;
+ 				}
+ 			} 
+
+ 			if(j > currentRobotColumn && obstacles.d > j ) {
+ 				if(cell.hasClass("gaucheGras")) {
+ 					obstacles.d = j;
+ 				} 
+ 				if(cell.hasClass("droiteGras")) {
+ 					obstacles.d = j+1;
+ 				}
+ 				if(hasRobot(cell)) {
+ 					obstacles.d = j
+ 				}
+ 			}
+ 			if(j==currentRobotColumn) {
+ 				if(cell.hasClass("gaucheGras")) {
+ 					obstacles.g = j-1;
+ 				} 
+ 				if(cell.hasClass("droiteGras")) {
+ 					obstacles.d = j+1;
+ 				}
+
+ 			}
+ 		}
+ 	
+
+ 	return obstacles;
+ }
+ 
+ function getNextPosition(direction) {
+
+ 	var obstacles = getObstacles();
+	var nextCell;
+	if(direction == "up") {
+		nextCell = $("#i"+(obstacles.h+1)+"_j"+currentRobotColumn);
+		//nextCell.css('background-color', 'black');
+	} else if(direction == "down") {
+		nextCell = $("#i"+(obstacles.b-1)+"_j"+currentRobotColumn);
+		//nextCell.css('background-color', 'black');
+	} else if (direction == "right") {
+		nextCell = $("#i"+currentRobotLine+"_j"+(obstacles.d-1));
+		//nextCell.css('background-color', 'black');
+	} else if (direction == "left") {
+		nextCell = $("#i"+currentRobotLine+"_j"+(obstacles.g+1));
+		//nextCell.css('background-color', 'black');
+	}
+	return nextCell;
+ }
+
+
+ function getRobotColor(l, c) {
+ 	for(var i=0; i<game.robots.length; i++) {
+ 		//console.log(game.robots[i].line);
+ 		//console.log(game.robots[i].column);
+ 		if(game.robots[i].line == l && game.robots[i].column == c) {
+ 			return game.robots[i].color;
+ 		} 	
+ 	}
+ 	return "";
+ }
+  
+ function marquerRobot(id, color) {
+ 	$("#"+id).css('background-color', color);
+ }	
+ 
+ function demarquerRobot(id) {
+ 	$("#"+id).css('background-color', 'white');
+ }
+
+ function sendProposition(color, l, c) {
+ 	console.log('l : '+l+' c:'+c);
+
+ 	$('#proposition').html('Transfert du robot '+color+' à ligne '+l+' et colonne '+c);
+
+	XHR("POST" ,"/proposition"
+ 		, { onload : function() {
+ 			console.log(this);
+ 			console.log(robots);
+ 			console.log(this.response);
+ 			var response = JSON.parse(this.response);
+ 			$('#serverresponse').html(this.response);
+ 			if(response.state != 'INVALID_MOVE') {
+ 				resetCells();
+	 			demarquerRobot(currentRobotCellId);
+	 			currentRobotCellId = "i"+l+"_j"+c; 
+		    	currentRobotLine = l;
+		    	currentRobotColumn = c;
+		    	updateRobotsPositions();
+		    	var nextCell = $('#i'+l+'_j'+c);
+		    	moveRobot(nextCell);	
+		    	marquerRobot(currentRobotCellId, currentRobotColor);
+ 			}	
+ 		}
+ 			,variables: {
+				login:user 
+				, idGame:idG
+				, proposition : JSON.stringify([
+					{
+						command : 'select'
+						, robot: color
+					}, {
+						command:'move'
+						, line : l
+						, column : c
+					}])
+ 			}
+ 	});
+ 	
+ }	
+ 	
+
+
+
+
+
+
+
+
+
+  
+  // retourne id de cellule du robot actuel
+
+  function getRobotActuelCellId() {
+  	var cellId; // id de la case du robot actuel
+ 		
+  	if(robotActuel) {
+  		var colorRobot = game.robots[robotActuel].color; // couleur du robot actuel
+ 		if(colorRobot == 'blue') {
+ 			cellId = "i"+lb+"_j"+cb;
+ 		} else if (colorRobot == 'green') {
+ 			cellId = "i"+lv+"_j"+cv;
+ 		} else if (colorRobot == 'red') {
+ 			cellId = "i"+lr+"_j"+cr;
+ 		} else if (colorRobot == 'yellow') {
+ 			cellId = "i"+lj+"_j"+cj;
+ 		}
+  		
+  	}
+  	return cellId;	
+  }
+
+
+ function addKeyboardEvent() {
+ 	if(robotActuel) {
+ 		// si la case du robot actuel est sélectionnée : 
+ 		$( "#"+getRobotActuelCellId()).keydown(function( event ) {
+ 			//console.log(event);
+ 		});
+ 	}
+ }
+
+
+  function initPositions() {
+  	for(var i=0; i<game.robots.length; i++) {
+		if(robots[i].color == 'blue') {
+			lb = robots[i].line;
+			cb = robots[i].column;
+		} else if(robots[i].color == 'red') {
+			lr = robots[i].line;
+			cr = robots[i].column;
+		} else if(robots[i].color == 'green') {
+			lv = robots[i].line;
+			cv = robots[i].column;
+		} else if(robots[i].color == 'yellow') {
+			lj = robots[i].line;
+			cj = robots[i].column;
+		}
+	}
+  }
+
+ /*
+ 	Ajout des listeners de clavier sur les robots
+ */ 	
+ function initDeplacements() {
+ 	addCellsListener();
+ 	initKeyboard();
+ 	//if(!robotActuel) { // la premiere fois on doit initialiser toutes les variables des robots
+ 	//	initPositions(); // remplie les variables de position de tous les robots
+ 	//}
+	//marquerRobot(); // dessine sa couleur sur la grille
+	//addKeyboardEvent();
+  /*
+  $("#robot_yellow").click(function(){
+        var image = $("#robot_yellow");
+        image.remove();
+   		$("#i0_j5").append(image); 
+   });
+  */
+ }
+
+ //initClick();
