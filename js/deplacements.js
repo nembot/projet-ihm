47ff73b6acf8a7ed616d091e43e07686adf21d14
massi <massi@success2i.com>
2015-01-05 12:22:40 +0100

    discard

--- js/deplacements.js
+++ js/deplacements.js
@@ -53,7 +53,6 @@ function setRobotByEvent(e) {
    // listen to events...
    mc.on("pan panleft panright panup pandown", function(ev) {
    	 setRobotByEvent(ev);
-
    	 if(ev.type == "panup" || ev.type == "pandown" ||ev.type == "panright" ||ev.type == "panleft")
  			//e.preventDefault();
 	    if(currentRobotColor != "") {
