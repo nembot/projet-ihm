2d2fe8aa120514556a43036a3071f06f1722dab0
toufik <toufik@success2i.com>
2015-01-08 10:54:28 +0100

    commit bug fisheye

--- js/fisheye.js
+++ js/fisheye.js
@@ -3,12 +3,17 @@
 	  $("#fisheye-div").fadeIn();
 	  //alert(e.target.id);
 	  console.log('targe' + e.target.id);
-	  if(e.target.id != null && e.target.id != '')
-	  	showGame("fisheye-div", ''+e.target.id+'');
+	  if(e.target.id != null && e.target.id != '') {
+		  	$("#fisheye-div").show();
+		  	showGame("fisheye-div", ''+e.target.id+'');
+		  	
+	  }
 	}); 
 
 	$('#FisheyeMenu').on('mouseout', '.fisheye-image', function() {
 	    $('#fisheye-div').empty();
+	    $("#fisheye-div").hide();
+		  	
 	    $("#fisheye-div").fadeOut();      
 	});
 }); 
